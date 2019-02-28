9ca3209b67090c295574bf087e8722ba0533f2f3
Batin Eryilmaz <btnbtn96@gmail.com>
2019-02-28 15:55:43 +0300

    ✨  search lower case insensitive

--- routes/index.js
+++ routes/index.js
@@ -251,8 +251,8 @@ router.post("/", async (req, res) => {
 router.post("/search-results", async(req,res) =>{
 
   if(req.body.searchText){
-      const searchText = req.body.searchText
-      const query = Post.find({ postTitle  :{ $regex: '.*' + searchText + '.*' }  }).exec()
+      const searchText = new RegExp(req.body.searchText, 'i')
+      const query = Post.find({ postTitle  :{ $regex: new RegExp('.*' + req.body.searchText + '.*', "i") }  }).exec()
       const post = await query.catch(err => res.send("something is wrong"))
       console.log(post);
       if(!(post.length < 1)){
